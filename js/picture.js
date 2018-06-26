681927e9e23352fadd7671225961aac367cf8217
Darkmus <darkmus@tut.by>
2018-06-26 12:08:05 +0300

    Make changes

--- js/picture.js
+++ js/picture.js
@@ -9,7 +9,7 @@
     tempCard.querySelector('.picture__img').setAttribute('src', cardsInfo.url);
     tempCard.querySelector('.picture__stat--likes').textContent = cardsInfo.likes;
     tempCard.querySelector('.picture__stat--comments').textContent = cardsInfo.comments.length;
-    return tempCard;
+    return tempCard.querySelector('a.picture__link');
   };
 
   window.createPicture = createCard;
